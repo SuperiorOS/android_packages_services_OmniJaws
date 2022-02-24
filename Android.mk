785adc89f4ed3b65e460c8fb15c5804ae39902d4
Semavi Ulusoy <doc.divxm@gmail.com>
2022-02-25 00:11:32 +0300

    OmniJaws: move to /system_ext
    Change-Id: I1390842d8c75210a8edc31011478e0d71ee51be9

--- Android.mk
+++ Android.mk
@@ -20,7 +20,10 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 LOCAL_PACKAGE_NAME := OmniJaws
 LOCAL_MODULE_TAGS := optional
-LOCAL_SDK_VERSION := system_current
+LOCAL_PRIVATE_PLATFORM_APIS := true
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_CERTIFICATE := platform
+LOCAL_SYSTEM_EXT_MODULE := true
 LOCAL_REQUIRED_MODULES := privapp_whitelist_org.omnirom.omnijaws-ext.xml
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
@@ -35,6 +38,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := privapp_whitelist_org.omnirom.omnijaws-ext.xml
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_MODULE_PATH := $(TARGET_OUT_SYSTEM_EXT_ETC)/permissions
+LOCAL_SYSTEM_EXT_MODULE := true
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
